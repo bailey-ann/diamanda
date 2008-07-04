efd72bb19aa69a4d83e5d276842a8efdc869b1da
riklaunim <riklaunim@85d0068e-3e1c-0410-b905-29a38a2b45fb>
2008-07-04 11:10:42 +0000

    adding stats as pagestats

---
+++ trunk/diamandas/pagestats/URLconf.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import *
+
+# Wiki* URLs
+urlpatterns = patterns('pagestats',
+(r'^/?$', 'views.mainpage'),
+(r'^entries/$', 'views.entries'),
+(r'^refs/$', 'views.referers'),
+(r'^google/$', 'views.google'),
+(r'^delete/$', 'views.delete_all'),
+)
---
+++ trunk/diamandas/pagestats/statsMiddleware.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# Diamanda Application Set
+# Simple stats
+
+from pagestats.models import *
+from datetime import datetime
+from django.http import HttpResponse, HttpResponseRedirect
+#from django.db import connection
+#from django.template import Template, Context
+
+class statsMiddleware(object):
+	"""
+	Log stats data
+	"""
+	def process_request(self, request):
+		if 'HTTP_USER_AGENT' in request.META:
+			a = request.META['HTTP_USER_AGENT'].lower()
+		else:
+			a = '--'
+		if a.find('httrack') != -1 or a.find('teleport') != -1 or a.find('wget') != -1 or a.find('copier') != -1:
+			return HttpResponse('Bez klonowania strony!')
+		if not 'stats' in request.session:
+			try:
+				today = str(datetime.today())[:10]
+				unique = Stat.objects.filter(ip = request.META['REMOTE_ADDR'], date = today).count()
+				if unique < 1:
+					if not request.META.has_key('HTTP_REFERER'):
+						request.META['HTTP_REFERER'] = ''
+					else:
+						request.META['HTTP_REFERER'] = request.META['HTTP_REFERER'].decode('utf-8')
+					s = Stat(ip = request.META['REMOTE_ADDR'], referer = request.META['HTTP_REFERER'], date = today)
+					s.save()
+			except:
+				pass
+			request.session['stats'] = 'ok'
